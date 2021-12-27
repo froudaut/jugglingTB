610d26b27ac989f91689e67ab6f4b672293e6c43
Fred Roudaut <roudaut.frederic@free.fr>
2021-12-27 23:26:27 +0100

    Linux Version

--- __compile.sh
+++ __compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo pp -o jugglingTB.bin jugglingTB.pl WebServer.pl -M File::Path -M Devel::Symdump  -M Set::Scalar -M Parse::Yapp -M Perl4::CoreLibs -M Term::ReadKey -M File::Grep -M File::Copy::Recursive -M Chart::Gnuplot -M HTTP::Server::Simple  -M List::MoreUtils -M Exporter::Tiny
+sudo pp -o jugglingTB.bin jugglingTB.pl WebServer.pl -M File::Path -M Devel::Symdump  -M Set::Scalar -M Parse::Yapp -M Perl4::CoreLibs -M Term::ReadKey -M File::Grep -M File::Copy::Recursive -M Chart::Gnuplot -M HTTP::Server::Simple 
